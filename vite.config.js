02bfacfcd4734783d08659bf054a73f778fe4d46
mawaos <gorrinmikel@gmail.com>
2025-04-13 20:48:07 -0400

    bug fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/wikosai/'
+  base: '/wikosai/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        mei: 'mei.html',
+        wikos: 'chat.html',
+      },
+    },
+  },
 })
